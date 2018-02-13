a8f852ea3c835a88a9a47cb26f936cefcecd79f8
Leonardo Tolosa <tolosa@gmail.com>
2018-02-13 18:43:06 -0300

    Add flex sizing example https://facebook.github.io/react-native/docs/height-and-width.html

--- App.js
+++ App.js
@@ -55,6 +55,19 @@ class Blink extends Component {
   }
 }
 
+// TODO: find a way to show in main page without breaking layout
+class FlexBasics extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    )
+  }
+}
+
 // TODO: learn how to manage styles across different controls
 const styles = StyleSheet.create({
   container: {
