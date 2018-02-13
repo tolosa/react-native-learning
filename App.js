3ffef96a5dea149cd4c411bfb2a6677f2eadef9e
Leonardo Tolosa <tolosa@gmail.com>
2018-02-13 16:26:19 -0300

    Add props sample From https://facebook.github.io/react-native/docs/props.html

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class ReactNativeLearning extends React.Component {
+export default class ReactNativeLearning extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello world!</Text>
+        <Bananas />
       </View>
+    );
+  }
+}
+
+class Bananas extends Component {
+  render() {
+    let image = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={image} style={{width: 193, height: 110}}/>
     );
   }
 }
