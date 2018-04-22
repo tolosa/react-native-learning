e479f33fe32a22c5abaa4a6b74dc869d647e2cda
Leonardo Tolosa <tolosa@gmail.com>
2018-04-21 22:29:03 -0300

    Add titles to navigation screens

--- app/components/Banana.js
+++ app/components/Banana.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Image } from 'react-native';
 
 export default class Banana extends Component {
+  static navigationOptions = {
+    title: 'Banana demo',
+  }
   render() {
     let image = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
--- app/components/Greeting.js
+++ app/components/Greeting.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class Greeting extends Component {
+  static navigationOptions = {
+    title: 'Greeting demo',
+  }
   render() {
     return (
       <Text>Hello {this.props.navigation.state.params.name}!</Text>
