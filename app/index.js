d939e0ac2a907acfbc4b928cca6796a8502b8ada
Leonardo Tolosa <tolosa@gmail.com>
2018-04-22 14:48:33 -0300

    Add descriptions to navigation list

--- app/index.js
+++ app/index.js
@@ -11,15 +11,38 @@ import PizzaTranslator from './components/PizzaTranslator';
 import FlexBasics from './components/FlexBasics';
 
 // TODO: duplicated values, populate from StackNavigator data
-const navListData = [{key: 'ButtonDemo'}, {key: 'Banana'}, {key: 'Blink'}, {key: 'PizzaTranslator'}, {key: 'Greeting'}]
+const navListData = [
+  {
+    key: 'ButtonDemo',
+    description: 'Simple button and alert demo',
+  },
+  {
+    key: 'Banana',
+    description: 'A SFW banana picture',
+  },
+  {
+    key: 'Blink',
+    description: 'Blinking text demo',
+  },
+  {
+    key: 'PizzaTranslator',
+    description: 'Translate text to pizza language',
+  },
+  {
+    key: 'Greeting',
+    description: 'Greets you',
+  },
+]
 
 class HomeScreen extends Component {
   renderItem = ({item}) => {
+    const screen = item.key
+    const description = item.description
     return (
       <ListItem
-        title={item.key}
-        subtitle='TODO: add subtitle here'
-        onPress={() => this.props.navigation.navigate(item.key)}
+        title={screen}
+        subtitle={description}
+        onPress={() => this.props.navigation.navigate(screen)}
       />
     )
   }
